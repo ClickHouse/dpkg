2a065a884b03d6aeab5e22ce325c7e5d2111c04e
Mikhail f. Shiryaev <mr.felixoid@gmail.com>
2021-12-23 17:47:56 +0100

    Update version

--- src/deb/main.c
+++ src/deb/main.c
@@ -52,7 +52,7 @@ printversion(const struct cmdinfo *cip, const char *value)
 {
   printf(_("Debian '%s' package archive backend version %s, from repo "
            "https://github.com/ClickHouse-Extras/dpkg.\n"),
-         BACKEND, "1.19.7-clickhouse");
+         BACKEND, "1.21.1-clickhouse");
   printf(_(
 "This is free software; see the GNU General Public License version 2 or\n"
 "later for copying conditions. There is NO warranty.\n"));
